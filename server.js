a58e50bcf7c3501f1e8c5aec470402c0e801c208
Matt Stjernholm <thew88@gmail.com>
2016-03-01 23:31:04 -0700

    implement request defaults for server

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Crowdsourcer';
 
 app.get('/', (request, response) => {
-  response.send('Welcome to Crowdsourcer!');
+  response.send(app.locals.title);
 });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
