794a8c7240152ab4663ee64f2b42f3663aa5b90d
Matt Stjernholm <thew88@gmail.com>
2016-03-02 12:44:29 -0700

    implement ejs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Crowdsourcer';
 
 app.get('/', (request, response) => {
-  response.send(app.locals.title);
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  response.render('pages/index');
 });
 
 if (!module.parent) {
